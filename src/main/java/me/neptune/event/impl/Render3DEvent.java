1470b4317b443349607ad8145771a8c9c0a74488
heedii <127703762+heedii@users.noreply.github.com>
2024-01-27 23:55:20 -0800

    Update Render3DEvent.java

--- src/main/java/me/neptune/event/impl/Render3DEvent.java
+++ src/main/java/me/neptune/event/impl/Render3DEvent.java
@@ -1,6 +1,6 @@
 package me.neptune.event.impl;
 
-import me.alpha432.oyvey.event.Event;
+import me.neptune.event.Event;
 import net.minecraft.client.util.math.MatrixStack;
 
 public class Render3DEvent extends Event {
