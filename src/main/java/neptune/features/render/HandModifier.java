396e0488ab50070081d17f840f2caf18676b7078
heedii <127703762+heedii@users.noreply.github.com>
2024-01-30 19:07:28 -0800

    handmodifier started

---
+++ src/main/java/neptune/features/render/HandModifier.java
@@ -0,0 +1,21 @@
+package neptune.features.render;
+
+import neptune.module.Mod;
+
+public class HandModifier extends Mod {
+    public HandModifier() {
+        super("HandModifier", "Modify your hand", Category.RENDER);
+    }
+
+
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
--- src/main/java/neptune/mixins/InGameHudMixin.java
+++ src/main/java/neptune/mixins/InGameHudMixin.java
@@ -1,7 +1,6 @@
 package neptune.mixins;
 
 import neptune.features.client.HUD;
-import neptune.ui.Hud;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
