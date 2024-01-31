6368b252eead8a1c0b110566d047d4532b63a008
heedii <127703762+heedii@users.noreply.github.com>
2024-01-30 17:34:58 -0800

    flight done

--- src/main/java/neptune/features/movement/Flight.java
+++ src/main/java/neptune/features/movement/Flight.java
@@ -1,5 +1,7 @@
 package neptune.features.movement;
 
+import neptune.event.EventManager;
+import neptune.event.events.TickEvent;
 import neptune.module.Mod;
 import neptune.module.settings.ModeSetting;
 import neptune.module.settings.NumberSetting;
@@ -7,19 +9,22 @@ import neptune.module.settings.NumberSetting;
 public class Flight extends Mod {
 
     private final ModeSetting mode = new ModeSetting("Mode", "Vanilla", "Vanilla");
-    private final NumberSetting speed = new NumberSetting("Speed", 1, 35, 5, 5);
+    private final NumberSetting speed = new NumberSetting("Speed", 0.1, 5, 5, 0.2);
 
     public Flight() {
         super("Flight", "Lets you fly (like a god)", Category.MOVEMENT);
         addSettings(mode, speed);
     }
 
-    @Override
-    public void onEnable() {
+    private final EventManager.EventListener<TickEvent> TickEventListener = e -> {
         if (mode.isMode("Vanilla")) {
             mc.player.getAbilities().setFlySpeed((float) this.speed.getValue());
             mc.player.getAbilities().flying = true;
         }
+    };
+
+    @Override
+    public void onEnable() {
     }
 
     @Override
