b02685978303b7f76292d1a6db404ba920249173
heedii <127703762+heedii@users.noreply.github.com>
2024-01-30 16:55:16 -0800

    Create Flight.java

---
+++ src/main/java/neptune/features/movement/Flight.java
@@ -0,0 +1,9 @@
+package neptune.features.movement;
+
+import neptune.module.Mod;
+
+public class Flight extends Mod {
+    Flight() {
+        super("Flight", "Lets you fly (like a god)");
+    }
+}
