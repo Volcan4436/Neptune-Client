ddc54724cb3c67da54156d7a8593d6f2caa969b1
heedii <127703762+heedii@users.noreply.github.com>
2024-02-01 07:33:30 -0800

    Update ReverseStep.java

--- src/main/java/neptune/features/movement/ReverseStep.java
+++ src/main/java/neptune/features/movement/ReverseStep.java
@@ -13,7 +13,7 @@ import net.minecraft.util.math.BlockPos;
 import java.util.Objects;
 
 public class ReverseStep extends Mod {
-    
+
     private final BooleanSetting strict = new BooleanSetting("Strict", false);
     private final BooleanSetting timer = new BooleanSetting("Timer", false);
     private final NumberSetting fallDistance = new NumberSetting("Fall Distance", 0, 5, 3, 0.1);
