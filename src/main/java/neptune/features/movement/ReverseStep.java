2b25bf894a11680c7817b28da46efce554024196
heedii <127703762+heedii@users.noreply.github.com>
2024-02-01 07:33:08 -0800

    Update ReverseStep.java

--- src/main/java/neptune/features/movement/ReverseStep.java
+++ src/main/java/neptune/features/movement/ReverseStep.java
@@ -2,7 +2,9 @@ package neptune.features.movement;
 
 import meteordevelopment.orbit.EventHandler;
 import neptune.Neptune;
+import neptune.event.events.TickEvent;
 import neptune.module.Mod;
+import neptune.module.settings.BooleanSetting;
 import neptune.module.settings.ModeSetting;
 import neptune.module.settings.NumberSetting;
 import net.minecraft.block.Blocks;
@@ -11,11 +13,14 @@ import net.minecraft.util.math.BlockPos;
 import java.util.Objects;
 
 public class ReverseStep extends Mod {
-    private final ModeSetting mode = new ModeSetting("Mode", "Vanilla", "Vanilla");
+    
+    private final BooleanSetting strict = new BooleanSetting("Strict", false);
+    private final BooleanSetting timer = new BooleanSetting("Timer", false);
+    private final NumberSetting fallDistance = new NumberSetting("Fall Distance", 0, 5, 3, 0.1);
 
     public ReverseStep() {
         super("ReverseStep", "cmon bruh", Category.MOVEMENT);
-        addSettings(mode);
+        addSettings(strict, timer, fallDistance);
     }
 }
 
