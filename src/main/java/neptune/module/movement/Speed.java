22f6d0f674f3d80ed6626f9c58a5f855b0ac2497
heedii <127703762+heedii@users.noreply.github.com>
2024-02-02 05:09:30 -0800

    Speed but broken

--- src/main/java/neptune/module/movement/Speed.java
+++ src/main/java/neptune/module/movement/Speed.java
@@ -3,14 +3,28 @@ package neptune.module.movement;
 import neptune.module.Mod;
 import neptune.setting.BooleanSetting;
 import neptune.setting.ModeSetting;
+import neptune.setting.NumberSetting;
 
 public class Speed extends Mod {
-    private final ModeSetting mode = new ModeSetting("Mode", "NCP", "NCP", "StrictStrafe");
-    private final BooleanSetting useTimer = new BooleanSetting("Use Timer", false);
+    private final ModeSetting mode = new ModeSetting("Mode", "Vanilla", "Vanilla");
+    private final NumberSetting vanillaSpeed = new NumberSetting("Speed", 1, 10, 3, 0.1);
 
     public Speed() {
         super("Speed", "Automatically speeds you up.", Category.MOVEMENT);
         addSetting(mode);
-        addSetting(useTimer);
+        vanillaSpeed.setDependency(() -> mode.getMode().equals("Vanilla"));
+        addSetting(vanillaSpeed);
+    }
+
+    public void onEnable() {
+        if (mode.isMode("Vanilla")) {
+            mc.player.getAbilities().setWalkSpeed((float) this.vanillaSpeed.getValue());
+        }
+    }
+
+    public void onDisable() {
+        if (mode.isMode("Vanilla")){
+            mc.player.getAbilities().setWalkSpeed(1.0F);
+        }
     }
 }
