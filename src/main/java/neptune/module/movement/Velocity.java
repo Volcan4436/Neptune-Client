99c206e7c232d85295a6acfe43ea2e77f4cfcc1f
Volcan <78641040+Volcan4436@users.noreply.github.com>
2024-03-10 16:57:42 +1100

    Added Velocity

--- src/main/java/neptune/module/movement/Velocity.java
+++ src/main/java/neptune/module/movement/Velocity.java
@@ -2,28 +2,46 @@ package neptune.module.movement;
 
 import meteordevelopment.orbit.EventHandler;
 import neptune.event.events.PacketEvent;
+import neptune.event.events.TickEvent;
 import neptune.module.Mod;
+import neptune.setting.ModeSetting;
 import neptune.setting.NumberSetting;
+import neptune.utils.player.movement.DamageBoostUtil;
 
 public class Velocity extends Mod {
-
-    private final NumberSetting horizontal = new NumberSetting("Horizontal", -2, 2.5, 0, 0.1);
-    private final NumberSetting vertical = new NumberSetting("Vertical", -2, 2.5, 0, 0.1);
+    //mode
+    private final ModeSetting mode = new ModeSetting("Mode", "MultiServer", "MultiServer", "Dev");
 
     public Velocity() {
        super("Velocity", "No Knockback", Category.MOVEMENT);
-       addSettings(horizontal, vertical);
+       addSetting(mode);
     }
+
+    double VelZ = 0;
 
     @EventHandler
-    private void onQ(PacketEvent.Receive per) {
-        if (mc.player == null) {
-            return;
+    private final void onTickEvent(TickEvent event) {
+
+        if (mode.getMode() == "MultiServer") {
+            double VelX = 0;
+            double VelY;
+            VelY = mc.player.getVelocity().getY();
+
+            if (mc.player == null) {
+                return;
+            }
+
+            if (!DamageBoostUtil.isBoosting()) {
+                assert mc.player != null;
+                VelX = mc.player.getVelocity().getX();
+                VelY = mc.player.getVelocity().getY();
+                VelZ = mc.player.getVelocity().getZ();
+            }
+            if (DamageBoostUtil.isHurtTime()) {
+                assert mc.player != null;
+                mc.player.setVelocity(VelX * -0.8, VelY, VelZ * -0.8);
+            }
         }
-        //if (per)
+
     }
-
-
-
-
 }
