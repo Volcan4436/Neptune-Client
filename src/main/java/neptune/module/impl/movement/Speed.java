0f7eb37429362981c07d35f6095aca5b03c7e4e8
Volcan <78641040+Volcan4436@users.noreply.github.com>
2024-06-29 15:26:23 +1000

    Added more Speed Modes

--- src/main/java/neptune/module/impl/movement/Speed.java
+++ src/main/java/neptune/module/impl/movement/Speed.java
@@ -6,6 +6,7 @@ import neptune.module.api.Module;
 import neptune.module.api.ModuleInfo;
 import neptune.setting.impl.BooleanSetting;
 import neptune.setting.impl.ModeSetting;
+import neptune.setting.impl.NumberSetting;
 import neptune.utils.player.movement.MoveUtil;
 
 /*
@@ -28,8 +29,10 @@ public class Speed extends Module {
     double fallDistance;
     boolean isGrounded;
 
-    private final ModeSetting mode = new ModeSetting("Mode", "Strafe", "NCP", "Dev");
+    private final ModeSetting mode = new ModeSetting("Mode", "Legit", "Strafe", "Hypixel", "Spartan-Position", "Boost", "NCP", "Dev");
     private final BooleanSetting jump = new BooleanSetting("Jump").require(() -> mode.is("Strafe"));
+    //Boost Speed
+    private final NumberSetting boostSpeed = new NumberSetting("Boost Speed", 0.6, 0.01, 2, 0.01).require(() -> mode.is("Boost"));
 
     public Speed() {
         addSettings(mode, jump);
@@ -38,14 +41,71 @@ public class Speed extends Module {
     @Listen
     public void onTick(TickEvent event) {
         switch (mode.getMode()) {
+
+            //Todo: Add Legit Strafe (Rotation Snap Silent) || Add Lag Buffer (Increase Speed Slightly when Lagging to improve Movement)
+            case "Legit" -> {
+                mc.options.jumpKey.setPressed(false);
+                if (MoveUtil.isMoving() && mc.player.isOnGround()) {
+                    mc.player.jump();
+                }
+            }
+
+            //Todo: Add Speed Option
             case "Strafe" -> {
-                MoveUtil.setSpeed();
+                MoveUtil.setSpeed(); //Start to Strafe
                 if (jump.isEnabled()) {
                     mc.options.jumpKey.setPressed(false);
                     if (mc.player.isOnGround() && MoveUtil.isMoving())
                         mc.player.jump();
                 }
             }
+
+            //Todo: Add Combat Strafe || Stop in Water || Add Omni Sprint Bypass
+            case "Hypixel" -> {
+                mc.options.jumpKey.setPressed(false);
+                if (mc.player.isUsingItem()) return;
+                if (mc.player.isSneaking()) return;
+                if (mc.player.isCrawling()) return;
+                if (mc.player.isSwimming()) return;
+                if (MoveUtil.isMoving()) {
+                    if (mc.player.isOnGround()) {
+                        mc.player.jump();
+                        MoveUtil.setSpeed(0.485); //Does the Ground Strafe
+                    }
+                }
+            }
+
+            //Todo: Stop Phasing Issue
+            case "Spartan-Position" -> {
+                float yaw = mc.player.getYaw();
+                double radians = Math.toRadians(yaw);
+
+                // Calculate the offset based on the player's viewing direction
+                double xOffset = -Math.sin(radians) * 0.3; // Calculate X offset based on yaw
+                double zOffset = Math.cos(radians) * 0.3;  // Calculate Z offset based on yaw
+                mc.options.jumpKey.setPressed(false);
+                if (mc.player.horizontalCollision) return;
+                if (mc.player.isOnGround()) {
+                    if (mc.options.forwardKey.isPressed()) {
+                        mc.player.updatePosition(mc.player.getX() + xOffset, mc.player.getY(), mc.player.getZ() + zOffset);
+                    } else if (mc.options.backKey.isPressed()) {
+                        mc.player.updatePosition(mc.player.getX() - xOffset, mc.player.getY(), mc.player.getZ() - zOffset);
+                    } else if (mc.options.leftKey.isPressed()) {
+                        mc.player.updatePosition(mc.player.getX() + zOffset, mc.player.getY(), mc.player.getZ() - xOffset);
+                    } else if (mc.options.rightKey.isPressed()) {
+                        mc.player.updatePosition(mc.player.getX() - zOffset, mc.player.getY(), mc.player.getZ() + xOffset);
+                    }
+                }
+            }
+
+            case "Boost" -> {
+                mc.options.jumpKey.setPressed(false);
+                if (mc.player.hurtTime == 1) {
+                    mc.player.setVelocity(mc.player.getVelocity().getX() * boostSpeed.getValue(), mc.player.getVelocity().getY(), mc.player.getVelocity().getZ() * boostSpeed.getValue());
+                }
+            }
+
+            //Todo: Make this Fast
             case "NCP" -> {
                 mc.options.jumpKey.setPressed(false);
                 if (mc.player.isOnGround()) {
@@ -59,6 +119,7 @@ public class Speed extends Module {
                 if (ticks >= 3)
                     ticks = 0;
             }
+
             case "Dev" -> {
                 // README: Used for testing purposes
             }
